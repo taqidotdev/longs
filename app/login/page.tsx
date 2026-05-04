49f3dd28e48d600f43591626791727ab997055c1
Taqi <muhammad@taqi.dev>
2026-05-04 23:35:57 +0400

    feat: supabase + google auth

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,15 +2,18 @@
 import { useState } from "react";
 import { MoveLeft } from "lucide-react";
 import { redirect } from "next/navigation";
+import { createClient } from "@/lib/supabase/client";
 
 export default function Home() {
   const [email, setEmail] = useState("");
+
+  const supabase = createClient();
 
   return (
     <div className="flex flex-col justify-center items-center gap-7 mb-7">
       <div
         onClick={() => redirect("/")}
-        className="relative right-28 top-6 fill-secondary flex items-center gap-1 text-primary hover:text-secondary hover:fill-secondary hover:cursor-pointer duration-100"
+        className="relative right-28 top-6 link"
       >
         <MoveLeft size={16} className="inline" />
         Back
@@ -25,7 +28,10 @@ export default function Home() {
         placeholder="Email"
         onChange={(e) => setEmail(e.target.value)}
       />
-      <button className="peer-focus:hidden peer-not-placeholder-shown:hidden">
+      <button
+        onClick={() => supabase.auth.signInWithOAuth({ provider: "google" })}
+        className="peer-focus:hidden peer-not-placeholder-shown:hidden"
+      >
         <p>Login with Google</p>
       </button>
       <button className="hidden peer-focus:block peer-not-placeholder-shown:block">
